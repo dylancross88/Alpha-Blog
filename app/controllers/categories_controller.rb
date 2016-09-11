8890a105f9d55d3199976fe5d1c919aec83e77e9
dylancross88 <dylan.cross88@gmail.com>
2016-09-11 10:19:48 +0000

    Added integration tests for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,44 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
 
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
 
   def show
-
   end
-  
+
+  def destroy
+    if @category.destroy
+      flash[:danger] = "Category deleted successfully"
+      redirect_to categories_path
+    else
+      flash.now[:danger] = "Unable to delete category"
+      render 'index'
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
+    def set_category
+      Category.find(params[:id])
+    end
+
 end
