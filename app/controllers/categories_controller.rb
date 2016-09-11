c3fa5956e689afec191fc81e0a7a48e77e6def9e
dylancross88 <dylan.cross88@gmail.com>
2016-09-11 12:55:20 +0000

    Added m2m relationship between Article and Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
+  before_action :require_admin, except: [:index, :show]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -39,6 +40,13 @@ class CategoriesController < ApplicationController
 
     def set_category
       Category.find(params[:id])
+    end
+
+    def require_admin
+      if !logged_in? || (logged_in? and !current_user.admin?)
+        flash[:danger] = "Only admins can perform that action"
+        redirect_to categories_path
+      end
     end
 
 end
